e201461881dc13a40f95a9429df5fbf3b8045cc3
iecgerman <iec_german@hotmail.com>
2023-10-04 21:04:47 -0600

    Clase 17 - Docstrings

---
+++ 17_docstrings.py
@@ -0,0 +1,48 @@
+"""
+El docstring o la documentación está dividido en tres partes importantes que son las siguientes:
+
+    Primero se da una descripción clara y concisa de la función y su funcionamiento
+
+    En medio se agrega la descripción de los diferentes parámetros, su tipo, su nombre y que es lo que se espera de esos parámetros
+
+    Por ultimo se agrega que es lo que devuelve nuestra función
+
+"""
+
+"""
+Les dejo algunas buenas prácticas que encontré
+
+    Recordar que en Python se maneja la indentación para delimitar bloques de instrucciones.
+    Escribir los programas los más simple posible.
+    Comentar los programas, explicando el propósito, funcionamiento completo y el resultado
+    esperado.
+    Al inicio de cada función, agregar brevemente un comentario que explique el comportamiento
+    general de la función.
+    Definir el nombre de funciones, variables y constantes que mejor representen el contenido que
+    almacenarán.
+    No usar variables cuyo nombre carezca de significado descriptivo. Con un código legible y
+    nombres significativos, el código se va auto documentado.
+    Declarar variables en líneas separadas, posibilitando agregar una descripción de cada variable
+    mediante comentarios.
+    Definir el nombre de funciones, módulos y clases que mejor representen las acciones que
+    realizan.
+    Definir el tamaño de las sangrías para que sean regulares (consistentes) y no varíen a lo largo
+    del código, es decir, si el primer bloque ocupa como indentación una tabulación, el resto de
+    bloques deben ser indentados con una tabulación adicional por cada nivel, con eso se facilita la
+    lectura en cualquier editor de código.
+    Ser consistente al momento de utilizar un estándar para nombres largos, por ejemplo para una
+    variable que almacena cantidad de alumnos 'contador_alumnos’
+    Comentar cuando sea justo y necesario, usar los comentarios dentro de las funciones para
+    describir las variables (sólo cuando su utilidad sea potencialmente dudosa) y cuando existan
+    bloques de código difíciles de entender a primera vista; el exceso de comentarios vuelve ilegible
+    el código.
+    Definir variables locales al inicio de la implementación de cada función, como un bloque de
+    código bien separado del bloque que contenga las instrucciones ejecutables, ésta separación
+    puede consistir en una línea en blanco, o bien un comentario que denote la utilidad de cada
+    bloque.
+    Evitar la incorporación de más de una instrucción por línea. Esto reduce notoriamente la
+    legibilidad del código, ya que el programador habitualmente está acostumbrado a leer una
+    instrucción por línea.
+    Realizar la indentación necesaria, si una instrucción abarca más de una línea.
+
+"""

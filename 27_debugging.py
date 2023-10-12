763b8391be9110fc9952a731f2196f952b8b8a7c
iecgerman <iec_german@hotmail.com>
2023-10-11 18:26:24 -0600

    Clase 27 - Debugging

---
+++ 27_debugging.py
@@ -0,0 +1,23 @@
+"""
+``En un curso llamado “Learning How TO Learn” explican que durante el sueño y el ejercicio el cerebro “barre” la información basura. Dejando únicamente la información útil para los contextos en los que nos desenvolvemos.
+
+Es un proceso fisiológico donde el líquido del cerebro literalmente se “depura” de conexiones neuronales inútiles. Y por eso es importante dormir bien y hacer ejercicio. También una sana alimentación importante el consumo de omegas, facilitan esos procesos en el cerebro. Es como que el cerebro hace su propio “debugging”.
+"""
+
+"""
+Hay un método de depuración llamado el patito de goma el cual consiste en explicarle tu código a un patito de goma hahaha.
+"""
+
+
+"""
+Llevaba dos días sin dormir y no encontraba la solución a un bug, estaba desarrollando una página web. Me aburrí, me frustré, y dije
+
+– Creo que ha llegado el momento de retirarme como desarrollador
+
+Me fui a dormir. Después de despertar, lo resolví en quince minutos. Duerman y duerman muy bien xD
+"""
+
+"""
+La razón por la cual nos vemos a dormir y obtenemos la respuesta es devido a que cuando estamos durmiendo, haciendo ejercicio o caminando, el cerebro entra en ‘Modo Difuso’, un modo en el cual el cerebro es capáz de realizar conexiones de ideas separadas. Este modo es totalemente distinto al ‘Modo Enfocado’, cuando estamos pensando en resolver un problema de programación.
+"""
+
